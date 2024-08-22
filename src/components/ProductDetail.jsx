31fbf908d4a03c120cf043042da6c861fdeaeb5a
Edwin <alexisgiljimenez@gmail.com>
2024-08-22 10:19:21 -0500

    descripción productos

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,7 +1,20 @@
+import '../styles/ProductDetail.css'
 
 export const ProductDetail = () => {
   return (
-    <div>ProductDetail</div>
+    <>
+      <div className="product-detail">
+        <div className='title'>
+          <h2>Detalles del Producto</h2>
+        </div>
+        <div className="info">
+          <p>Aquí va la descripción del producto que va a hacer Cuervo</p>
+        </div>
+        <div className='precio'>
+          <b>Precio: $1000</b>
+        </div>
+      </div>
+    </>
   )
 }
 export default ProductDetail;
