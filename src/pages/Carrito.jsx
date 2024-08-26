16a6c07ec21dd9147bcf671ebbd37eb93b9d111b
AcevedoYeison <yeisonalexanderacevedocardona@gamil.com>
2024-08-26 08:11:50 -0600

    .l.

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,32 +1,11 @@
-import '../styles/carrito.css';
+import Cart from "../components/Cart";
+
 
 function Carrito() {
-  return (
-    <div className="carrito">
-      <h2>Tu Carrito de Compras</h2>
-      <div className="carrito-items">
-        {/* Reemplaza este bloque con un mapeo de productos */}
-        <div className="carrito-item">
-          <img src="../../public/vite.svg" alt="Producto" />
-          <div className="carrito-item-details">
-            <h3>Nombre del Producto</h3>
-            <p>Precio: $100</p>
-            <div className="cantidad">
-              <button>-</button>
-              <input type="number" value="1" />
-              <button>+</button>
-            </div>
-            <button className="eliminar">Eliminar</button>
-          </div>
-        </div>
-        {/* Fin del bloque de ejemplo */}
-      </div>
-      <div className="carrito-total">
-        <h3>Total: $100</h3>
-        <button className="comprar">Proceder a la Compra</button>
-      </div>
-    </div>
-  );
+  return(
+<Cart/>
+
+  )
 }
 
 export default Carrito;
