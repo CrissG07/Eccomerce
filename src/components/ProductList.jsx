4c5258ab50fee0d8145009eeede86e47f3049cad
Edwin <alexisgiljimenez@gmail.com>
2024-08-26 10:27:28 -0500

    Barra de categorías en los productos

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -9,6 +9,20 @@ const ProductList = ({ products }) => {
 
     return (
         <div>
+            <div className='section'>
+                <ul className='categoria'>
+                    <a className='link' href="#"><li className='list'>Moda</li></a>
+                    <a className='link' href="#"><li className='list'>Electrónicos</li> </a>                   
+                    <a className='link' href="#"><li className='list'>Hogar</li></a>
+                    <a className='link' href="#"><li className='list'>Deportes</li></a>
+                    <a className='link' href="#"><li className='list'>Juguetes</li></a>
+                    <a className='link' href="#"><li className='list'>Belleza</li></a>
+                    <a className='link' href="#"><li className='list'>Herramientas</li></a>
+                    <a className='link' href="#"><li className='list'>Mascotas</li></a>
+                    <a className='link' href="#"><li className='list'>Comida</li></a>
+                    <a className='link' href="#"><li className='list'>Más</li></a>
+                </ul>
+            </div>
             <div className="product-list">
                 {products.map((product) => (
                     <div key={product.id} onClick={() => setSelectedProduct(product)}>
