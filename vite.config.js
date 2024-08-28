83c23f517a129561ee35d9467c0b957a7f8812fe
Cristian <cristiangomez0607@gmail.com>
2024-08-28 10:57:36 -0500

    si

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Cambia '/Eccomerce/' al nombre de tu repositorio
+  base: '/Eccomerce/', // Configura esto según el nombre de tu repositorio
 });
+
